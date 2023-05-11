e114ac6086ed5015a69997d07f35b92552e7aa2a
EAG <daviddlm@gmail.com>
2023-05-11 14:22:44 +0200

    ferfe

---
+++ prueba_2/src/prueba_2/Prueba_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_2;
+
+/**
+ *
+ * @author EAG
+ */
+public class Prueba_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.2ergjibweoriyfbhgfgfwebyrgbyuwergbyweruigbweuirgiwergio23nrg23hugr
+    }
+    
+}
